6556d9110642253323af97a67d6de08ed718d92d
李弘唯 <bbeenn1227@gmail.com>
2021-12-26 23:51:52 +0800

    Improve My Shitty Code.

--- src/main/java/io/github/bloodnighttw/WhateverBot/VoiceChannelAutoCraft/VCAutoCraftEventHandler.java
+++ src/main/java/io/github/bloodnighttw/WhateverBot/VoiceChannelAutoCraft/VCAutoCraftEventHandler.java
@@ -3,54 +3,57 @@ package io.github.bloodnighttw.WhateverBot.VoiceChannelAutoCraft;
 import net.dv8tion.jda.api.entities.AudioChannel;
 import net.dv8tion.jda.api.entities.Category;
 import net.dv8tion.jda.api.entities.VoiceChannel;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
+import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.hooks.EventListener;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
 
-public class VCAutoCraftEventHandler extends ListenerAdapter {
+public class VCAutoCraftEventHandler implements EventListener {
 
 	private final Logger logger = LoggerFactory.getLogger(VCAutoCraftEventHandler.class);
-	private HashSet<AudioChannel> vcBotCreated;
+	private final HashSet<AudioChannel> vcBotCreated;
 
 	public VCAutoCraftEventHandler() {
 		vcBotCreated = new HashSet<>();
 	}
 
 	@Override
-	public void onGuildVoiceJoin(@NotNull GuildVoiceJoinEvent event) {
+	public void onEvent(@NotNull GenericEvent event) {
 
+		if (event instanceof GuildVoiceUpdateEvent) {
+			onGuildVoiceUpdate((GuildVoiceUpdateEvent) event);
+		}
 
 	}
 
-
-	@Override
 	public void onGuildVoiceUpdate(@NotNull GuildVoiceUpdateEvent event) {
 
 		if (event.getChannelJoined() != null) {
 
 			if (event.getChannelJoined().getId().equals(System.getenv("VC_ID"))) {
+
 				L1:
 				for (Category it : event.getGuild().getCategories()) {
-					for (VoiceChannel vcit : it.getVoiceChannels()) {
-						if (vcit.getId().equals(System.getenv("VC_ID"))) {
+					for (VoiceChannel voiceChannelIterator : it.getVoiceChannels()) {
 
-							logger.debug(vcit.getId() + "  " + System.getenv("VC_ID") + "  " + vcit.getId().equals(System.getenv("VC_ID")));
+						if (voiceChannelIterator.getId().equals(System.getenv("VC_ID"))) {
+
+							logger.debug(voiceChannelIterator.getId() + "  " + System.getenv("VC_ID") + "  " + voiceChannelIterator.getId().equals(System.getenv("VC_ID")));
 
 							it.createVoiceChannel(" + " + event.getMember().getUser().getName() + " + ").queue(
 									voiceChannel -> {
 										voiceChannel.getGuild().moveVoiceMember(event.getMember(), voiceChannel).queue();
 										vcBotCreated.add(voiceChannel);
 										logger.debug("create channel for user" + event.getMember().getUser().getName() + " in " + voiceChannel.getId());
-									}
-							);
+									});
+
 							break L1;
 						}
+
 					}
 				}
 
@@ -68,11 +71,7 @@ public class VCAutoCraftEventHandler extends ListenerAdapter {
 
 		}
 
-
 	}
 
-	@Override
-	public void onGuildVoiceLeave(@NotNull GuildVoiceLeaveEvent event) {
 
-	}
 }
