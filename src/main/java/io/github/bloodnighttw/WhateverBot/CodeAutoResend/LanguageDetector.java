36ed66ea9a4241dc7f390ef0977431c6464b300c
李弘唯 <bbeenn1227@gmail.com>
2022-01-05 21:24:42 +0800

    improve programing detection.

--- src/main/java/io/github/bloodnighttw/WhateverBot/CodeAutoResend/LanguageDetector.java
+++ src/main/java/io/github/bloodnighttw/WhateverBot/CodeAutoResend/LanguageDetector.java
@@ -1,15 +1,17 @@
 package io.github.bloodnighttw.WhateverBot.CodeAutoResend;
 
+import java.util.Arrays;
+
 public class LanguageDetector {
 
 	private final static String JAVA = "public static void main(String[]";
-	private final static String CPP = "#incluide <iostream>";
+	private final static String[] CPP = {"#incluide <iostream>", "#include <bits/stdc++.h>"};
 	private final static String C = "#include <stdio.h>";
 
 	public static Language detectCode(String code) {
 		if (code.contains(JAVA))
 			return Language.Java;
-		if (code.contains(CPP))
+		if (Arrays.stream(CPP).anyMatch(code::contains))
 			return Language.CPP;
 		if (code.contains(C))
 			return Language.C;
