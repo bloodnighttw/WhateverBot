06142e67be32243f927ff72a00dc8c05569a5570
李弘唯 <bbeenn1227@gmail.com>
2022-01-06 16:29:57 +0800

    refactor LanguageDetector.kt.

--- src/main/java/io/github/bloodnighttw/whateverBot/codeWrapper/MessageHandler.kt
+++ src/main/java/io/github/bloodnighttw/whateverBot/codeWrapper/MessageHandler.kt
@@ -4,15 +4,23 @@ import io.github.bloodnighttw.whateverBot.codeWrapper.Language.Other
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.EventListener
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class MessageHandler : EventListener {
+
+    val logger: Logger = LoggerFactory.getLogger(MessageHandler::class.java)
+
     override fun onEvent(event: GenericEvent) {
         if (event is MessageReceivedEvent) {
             val lang = detectCode(event.message.contentRaw)
+            logger.debug(lang.toString())
+
 
             if (event.author.isBot || event.message.contentRaw.contains("```") || lang == Other) return
 
             event.channel.sendMessage("```" + lang + "\n" + event.message.contentRaw + "```").queue()
+
         }
     }
 }
